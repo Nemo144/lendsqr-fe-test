0a211b9bd68c45bb3b250f95098663291fce2a69
Nnamdi Okeke <onnamdialex@gmail.com>
2023-01-04 13:46:00 +0100

    :art: Improve structure and format of the code

---
+++ lendsqr-fe-test/src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Dashboard } from "./Dashboard/Dashboard";
+export { default as Login } from "./Login/Login";
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as User } from "./User/User";
+export { default as UserDetails } from "./UserDetails/UserDetails";
