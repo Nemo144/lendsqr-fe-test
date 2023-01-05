b41322b16600b965a3e794fb87cb113ff5f8e46a
Nnamdi Okeke <onnamdialex@gmail.com>
2023-01-05 19:26:31 +0100

    :lipstick: update style file login

--- lendsqr-fe-test/src/components/Login/Login.tsx
+++ lendsqr-fe-test/src/components/Login/Login.tsx
@@ -1,16 +1,43 @@
 import React from "react";
 import { images } from "../../constants";
+import "./login.scss";
 
 const Login = () => {
   return (
-    <div>
-      <div>
-        <div>
-          <img src={images.logo} alt="logo" />
+    <div className="login">
+      <div className="login-content">
+        <div className="login-pictures">
+          <div>
+            <img src={images.logo} alt="logo" />
+          </div>
+
+          <div>
+            <img src={images.pablo} alt="pablo" />
+          </div>
         </div>
 
         <div>
-          <img src={images.pablo} alt="pablo" />
+          <h1>Welcome!</h1>
+          <p>Enter details to login</p>
+          <div>
+            <div>
+              <input type="email" placeholder="Email" name="email" value="" />
+            </div>
+
+            <div>
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                value=""
+              />
+              <p>SHOW</p>
+            </div>
+          </div>
+
+          <p>Forgot Password?</p>
+
+          <button type="button">LOG IN</button>
         </div>
       </div>
     </div>
