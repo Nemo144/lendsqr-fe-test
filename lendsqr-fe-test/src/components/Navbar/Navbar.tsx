740c5f555d32509445c637cf799b1d50d5c983c9
Nnamdi Okeke <onnamdialex@gmail.com>
2023-01-24 14:29:02 +0100

    :construction: navbar tsx done. now scss wip...

--- lendsqr-fe-test/src/components/Navbar/Navbar.tsx
+++ lendsqr-fe-test/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { images } from "../../constants";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div>
+        <img src={images.logo} alt="logo" />
+      </div>
+
+      <div>
+        <input
+          type="search"
+          placeholder="search for anything"
+          name="search"
+          id="search"
+        />
+      </div>
+
+      <div>
+        <a href="/">Docs</a>
+      </div>
+
+      <div>🔔</div>
+
+      <div>
+        <img src={images.avatar} alt="avatar" />
+        <h5>adedeji</h5>
+        <>˯</>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- lendsqr-fe-test/src/constants/images.js
+++ lendsqr-fe-test/src/constants/images.js
@@ -1,7 +1,9 @@
 import logo from "../assets/logo.png";
 import pablo from "../assets/pablo.png";
+import avatar from "../assets/avatar.png";
 
 export default {
   logo,
   pablo,
+  avatar,
 };
