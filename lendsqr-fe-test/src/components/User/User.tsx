bb63245f1dab05ee748a04cde79022f18fee4d89
Nnamdi Okeke <onnamdialex@gmail.com>
2023-01-27 19:45:54 +0100

    :construction: wip

--- lendsqr-fe-test/src/components/User/User.tsx
+++ lendsqr-fe-test/src/components/User/User.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "../Navbar/Navbar";
 
 const User = () => {
-  return <div>User</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default User;
