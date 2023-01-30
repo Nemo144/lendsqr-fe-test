e693d8da7ad3491b76f80c29a3846203d3bc535d
Nnamdi Okeke <onnamdialex@gmail.com>
2023-01-30 17:30:38 +0100

    :bug: routing bug fix

--- lendsqr-fe-test/src/App.tsx
+++ lendsqr-fe-test/src/App.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { Login, SignUp, Navbar } from "./components";
 import Routees from "./components/Routes";
 
 const App = () => {
   return (
     <div>
-      <Login />
-      {/* <Navbar /> */}
-      {/* <Routees /> */}
+      <Routees />
     </div>
   );
 };
